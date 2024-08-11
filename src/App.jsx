43600636a05219d1cfd7e634c2003850db199651
CODE_STUDIO <code@zoxs-MacBook-Air.local>
2024-08-11 08:24:08 +0530

    fix2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import DoctorPage from "./pages/doctor/DoctorPage";
 import PatientPage from "./pages/patient/PatientPage";
@@ -9,67 +9,42 @@ import Home from "./pages/Home/Home";
 import Researcher from "./pages/researcher/Researcher";
 import Professor from "./pages/professor/Professor";
 
-// A simple auth check function
-const isAuthenticated = () => !!localStorage.getItem('token');
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-// Protected route component
-const ProtectedRoute = ({ children }) => {
-  if (!isAuthenticated()) {
-    return <Navigate to="/login" replace />;
-  }
-  return children;
-};
+  useEffect(() => {
+    const checkAuth = () => {
+      const token = localStorage.getItem('token');
+      setIsAuthenticated(!!token);
+    };
 
-function App() {
+    checkAuth();
+    window.addEventListener('storage', checkAuth);
+
+    return () => {
+      window.removeEventListener('storage', checkAuth);
+    };
+  }, []);
+
+  const ProtectedRoute = ({ children }) => {
+    return isAuthenticated ? children : <Navigate to="/login" replace />;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
-        <Route 
-          path="/doctor" 
-          element={
-            <ProtectedRoute>
-              <DoctorPage />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/patient" 
-          element={
-            <ProtectedRoute>
-              <PatientPage />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/pathology" 
-          element={
-            <ProtectedRoute>
-              <PathologyPage />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/researcher" 
-          element={
-            <ProtectedRoute>
-              <Researcher />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/professor" 
-          element={
-            <ProtectedRoute>
-              <Professor />
-            </ProtectedRoute>
-          } 
-        />
+        <Route path="/doctor" element={<ProtectedRoute><DoctorPage /></ProtectedRoute>} />
+        <Route path="/patient" element={<ProtectedRoute><PatientPage /></ProtectedRoute>} />
+        <Route path="/pathology" element={<ProtectedRoute><PathologyPage /></ProtectedRoute>} />
+        <Route path="/researcher" element={<ProtectedRoute><Researcher /></ProtectedRoute>} />
+        <Route path="/professor" element={<ProtectedRoute><Professor /></ProtectedRoute>} />
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
